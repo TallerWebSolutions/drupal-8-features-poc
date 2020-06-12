7f2c85a4ee35f7f9d4decf8090233fcc3386577c
marcoslehmkuhl <m.v.lehmkuhl@gmail.com>
2020-06-12 14:37:28 +0000

    WIP

--- web/modules/custom/news/src/tests/Kernel/NewsKernelTest.php
+++ web/modules/custom/news/src/tests/Kernel/NewsKernelTest.php
@@ -12,21 +12,36 @@ use Drupal\KernelTests\Core\Entity\EntityKernelTestBase;
  */
 class NewsListTest extends EntityKernelTestBase {
 
+  public static $modules = ['node', 'news'];
+
   /**
    * @var 
    */
   private $nodeStorage;
 
   public function setUp() {
-    $this->nodeStorage = $this->container->get('entity_type.manager')->getStorage($node);
+    parent::setUp();
+    $this->installConfig(['news']);
+    $this->nodeStorage = $this->container->get('entity_type.manager')->getStorage('node');
   }
 
   public function testShouldLoadNewsNode() {
 
     $myNode = $this->nodeStorage->create([
-      'name' => 'Minha notícia fabulosa'
+      'nid' => 1,
+      'title' => 'Minha notícia fabulosa',
+      'field_my_field' => 'My field',
+      'type' => 'foo'
     ]);
-    
-    $this->assert($myNode->name, 'Minha notícia fabulosa');
+
+    $myNode->save();
+
+    $nodes = $this->nodeStorage->loadByProperties([
+      'type' => 'foo'
+    ]);
+
+    $node = reset($nodes);
+
+    $this->assertEqual($node->get('field_my_field')->getValue(), 'My field');
   }
 }
