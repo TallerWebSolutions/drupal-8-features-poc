e9a499c4df9f01bff85252ad5ea50e37c654bf1c
tukka <arthur.almeidapereira@gmail.com>
2020-06-12 14:46:38 -0300

    WIP

--- web/modules/custom/news/src/tests/Kernel/NewsKernelTest.php
+++ web/modules/custom/news/src/tests/Kernel/NewsKernelTest.php
@@ -1,31 +1,37 @@
 <?php
+
 namespace Drupal\Tests\news\Kernel;
 
+use Drupal\Tests\graphql_core\Kernel\GraphQLCoreTestBase;
+use GraphQL\Server\OperationParams;
 use Drupal\KernelTests\Core\Entity\EntityKernelTestBase;
-
-
+use Drupal\Tests\graphql_core\Kernel\GraphQLContentTestBase;
+use Drupal\Tests\graphql\Kernel\GraphQLTestBase;
 
 /**
  * Tests for news list.
  *
  * @group nsc_news
  */
-class NewsListTest extends EntityKernelTestBase {
+class NewsKernelTest extends GraphQLContentTestBase
+{
 
-  public static $modules = ['node', 'news', 'menu_ui'];
+  public static $modules = ['node', 'news', 'menu_ui', 'graphql', 'graphql_core'];
 
   /**
    * @var \Drupal\node\NodeStorage $nodeStorage
    */
   private $nodeStorage;
 
-  public function setUp() {
+  public function setUp()
+  {
     parent::setUp();
     $this->installConfig(['news']);
     $this->nodeStorage = $this->container->get('entity_type.manager')->getStorage('node');
   }
 
-  public function testShouldLoadNewsNode() {
+  public function testShouldLoadNewsNode()
+  {
 
     $myNode = $this->nodeStorage->create([
       'nid' => 1,
@@ -44,7 +50,8 @@ class NewsListTest extends EntityKernelTestBase {
     $this->assertEqual($node->get('field_my_field')->getString(), 'My field');
   }
 
-  public function testNodeQuery() {
+  public function testNodeQuery()
+  {
 
     $nodes = [
       [
@@ -64,7 +71,7 @@ class NewsListTest extends EntityKernelTestBase {
       ]
     ];
 
-    foreach($nodes as $node){
+    foreach ($nodes as $node) {
       $this->nodeStorage->create($node)->save();
     }
 
@@ -72,5 +79,55 @@ class NewsListTest extends EntityKernelTestBase {
     $results = $query->condition('field_my_field', 'My Field')->execute();
 
     $this->assertEqual(count($results), 2);
-  } 
+  }
+
+  public function testNodeQueryGraphQL() {
+     $nodes = [
+      [
+        'title' => 'Minha notícia fabulosa 1',
+        'field_my_field' => 'My field',
+        'type' => 'foo'
+      ],
+      [
+        'title' => 'Minha notícia fabulosa 2',
+        'field_my_field' => 'My field',
+        'type' => 'foo'
+      ],
+      [
+        'title' => 'Minha notícia fabulosa 3',
+        'field_my_field' => 'My field 2',
+        'type' => 'foo'
+      ]
+    ];
+
+    foreach ($nodes as $node) {
+      $this->nodeStorage->create($node)->save();
+    }
+
+    $res = $this->executeQuery('nodeQuery.gql');
+
+    $this->assertEquals(count($res['data']), 3);
+  }
+  
+  /**
+   * Returns a result of a GraphQL query.
+   *
+   * @param string $query
+   *   A graphql query.
+   * @param array $variables
+   *   Query variables.
+   *
+   * @return string
+   *   Query result.
+   */
+  protected function executeQuery($query, array $variables = [])
+  {
+    return $this->graphQlProcessor()->processQuery(
+      $this->getDefaultSchema(),
+      OperationParams::create([
+        'query' => $query,
+        'variables' => $variables,
+      ])
+    );
+  }
 }
